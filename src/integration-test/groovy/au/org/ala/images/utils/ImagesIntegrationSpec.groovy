88b0eb3ab2aec8831b1184561302b8b6dd8f54d9
Simon Bear <simon.bear@csiro.au>
2023-03-29 12:24:26 +1100

    Disable disk thresholds for elasticsearch docker container

---
+++ src/integration-test/groovy/au/org/ala/images/utils/ImagesIntegrationSpec.groovy
@@ -0,0 +1,63 @@
+package au.org.ala.images.utils
+
+import grails.config.Config
+import org.grails.config.PropertySourcesConfig
+import org.grails.orm.hibernate.cfg.Settings
+import org.springframework.boot.env.PropertySourceLoader
+import org.springframework.core.env.MapPropertySource
+import org.springframework.core.env.MutablePropertySources
+import org.springframework.core.env.PropertySource
+import org.springframework.core.io.DefaultResourceLoader
+import org.springframework.core.io.Resource
+import org.springframework.core.io.ResourceLoader
+import org.springframework.core.io.support.SpringFactoriesLoader
+
+class ConfigUtils {
+
+    static Config getConfig() { // CHANGED extracted from setupSpec so postgresRule can access
+
+        List<PropertySourceLoader> propertySourceLoaders = SpringFactoriesLoader.loadFactories(PropertySourceLoader.class, ConfigUtils.class.getClassLoader())
+        ResourceLoader resourceLoader = new DefaultResourceLoader()
+        MutablePropertySources propertySources = new MutablePropertySources()
+        PropertySourceLoader ymlLoader = propertySourceLoaders.find { it.getFileExtensions().toList().contains("yml") }
+        if (ymlLoader) {
+            load(resourceLoader, ymlLoader, "application.yml").each {
+                propertySources.addLast(it)
+            }
+        }
+        PropertySourceLoader groovyLoader = propertySourceLoaders.find { it.getFileExtensions().toList().contains("groovy") }
+        if (groovyLoader) {
+            load(resourceLoader, groovyLoader, "application.groovy").each {
+                propertySources.addLast(it)
+            }
+        }
+        propertySources.addFirst(new MapPropertySource("defaults", getConfiguration()))
+        return new PropertySourcesConfig(propertySources)
+    }
+
+    // Changed: Made static for getConfig()
+    private static List<PropertySource> load(ResourceLoader resourceLoader, PropertySourceLoader loader, String filename) {
+        if (canLoadFileExtension(loader, filename)) {
+            Resource appYml = resourceLoader.getResource(filename)
+            return loader.load(appYml.getDescription(), appYml) as List<PropertySource>
+        } else {
+            return Collections.emptyList()
+        }
+    }
+
+    // Changed: Made static for getConfig()
+    private static boolean canLoadFileExtension(PropertySourceLoader loader, String name) {
+        return Arrays
+                .stream(loader.fileExtensions)
+                .map { String extension -> extension.toLowerCase() }
+                .anyMatch { String extension -> name.toLowerCase().endsWith(extension) }
+    }
+
+    /**
+     * @return The configuration
+     */
+    static Map getConfiguration() { // changed to static
+        Collections.singletonMap(Settings.SETTING_DB_CREATE,  (Object) "validate") // CHANGED from 'create-drop' to 'validate'
+    }
+
+}
