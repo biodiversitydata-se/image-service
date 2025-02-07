e696041ee68a9f1c5d96ea15de41f7319b699e71
Mats Bovin <mats.bovin@gmail.com>
2025-02-07 11:58:18 +0100

    Serve original image from original url

--- grails-app/domain/au/org/ala/images/SpaceSavingFileSystemStorageLocation.groovy
+++ grails-app/domain/au/org/ala/images/SpaceSavingFileSystemStorageLocation.groovy
@@ -1,5 +1,7 @@
 package au.org.ala.images
 
+import org.apache.commons.io.IOUtils
+
 class SpaceSavingFileSystemStorageLocation extends FileSystemStorageLocation {
 
     static constraints = {
@@ -7,6 +9,20 @@ class SpaceSavingFileSystemStorageLocation extends FileSystemStorageLocation {
 
     static mapping = {
         cache true
+    }
+
+    @Override
+    InputStream originalInputStream(String uuid, Range range) throws FileNotFoundException {
+        def image = Image.findByImageIdentifier(uuid, [cache: true])
+        log.info("originalInputStream: $uuid -> $image.originalFilename")
+        def inputStream= new URL(image.originalFilename).openStream()
+        return range?.wrapInputStream(inputStream) ?: inputStream
+    }
+
+    @Override
+    byte[] retrieve(String uuid) {
+        log.info("retrieve: $uuid")
+        IOUtils.toByteArray(originalInputStream(uuid, null))
     }
 
     @Override
